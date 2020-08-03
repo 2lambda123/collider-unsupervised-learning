8adb26c39d4217e5166f4c5d8be0e6c1603e4206
michaelmelquiades <54324820+michaelmelquiades@users.noreply.github.com>
2020-08-03 18:47:43 +0100

    Added single file functionality
    Will now accept a file path as an argument to the functions 'single_csv_to_df' and 'single_csv_to_np'.

--- phenom_data_read_in.py
+++ phenom_data_read_in.py
@@ -69,3 +69,72 @@ def csv_to_np(directory):
     col_names = np.array(col_names)
     
     return data, col_names
+
+############################################################################################
+def single_csv_to_df(path):
+    data = []    
+    with open(path, 'r') as file:
+            for line in file.readlines():
+                line = line.replace(';', ',')
+                line = line.rstrip(',\n')
+                line = line.split(',')
+                data.append(line)
+    
+    #Find the longest line in the data 
+    longest_line = max(data, key = len)
+        
+    #Set the maximum number of columns
+    max_col_num = len(longest_line)
+
+    #Set the columns names
+    col_names = ['event_ID', 'process_ID', 'event_weight', 'MET', 'MET_Phi']
+
+    for i in range(1, (int((max_col_num-5)/5))+1):
+        col_names.append('obj'+str(i))
+        col_names.append('E'+str(i))
+        col_names.append('pt'+str(i))
+        col_names.append('eta'+str(i))
+        col_names.append('phi'+str(i))
+
+    #Create a dataframe from the list, using the column names from before
+    df = pd.DataFrame(data, columns=col_names)
+    df.fillna(value=pd.np.nan, inplace=True)
+    
+    #Pickle the dataframe to keep it fresh
+    p_path = 'my_df.pkl'
+    df.to_pickle(p_path)
+    
+    return df
+
+
+############################################################################################
+def single_csv_to_np(path):
+    data = []    
+    with open(path, 'r') as file:
+            for line in file.readlines():
+                line = line.replace(';', ',')
+                line = line.rstrip(',\n')
+                line = line.split(',')
+                data.append(line)
+    
+    #Find the longest line in the data 
+    longest_line = max(data, key = len)
+        
+    #Set the maximum number of columns
+    max_col_num = len(longest_line)
+
+    #Set the columns names
+    col_names = ['event_ID', 'process_ID', 'event_weight', 'MET', 'MET_Phi']
+
+    for i in range(1, (int((max_col_num-5)/5))+1):
+        col_names.append('obj'+str(i))
+        col_names.append('E'+str(i))
+        col_names.append('pt'+str(i))
+        col_names.append('eta'+str(i))
+        col_names.append('phi'+str(i))
+
+    #Convert everything into numpy arrays
+    data = np.array(data)
+    col_names = np.array(col_names)
+    
+    return data, col_names
